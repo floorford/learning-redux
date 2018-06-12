871ec3b86e9bfcb091ca90e63c62d996dd6d07e2
Flo Orford <floorford@Flos-MacBook-Pro.local>
2018-06-12 10:06:06 +0100

    added some comments to the reducer file

--- src/reducer.js
+++ src/reducer.js
@@ -3,16 +3,27 @@ import initial from './initial.js'
 let increasePlayerScore = (state, { type }) => {
   const stateCopy = {...state};
 
+  // increases the scores by 1 respectively
   stateCopy[type] += 1;
 
+  // deciding who is serving
   let counter = stateCopy.player1 + stateCopy.player2
 
-  if (Math.floor(counter / 5) % 2 === 0) {
-    stateCopy.serving = true
+  if (stateCopy.player1 >= 21 && stateCopy.player2 >= 21) {
+    if (Math.floor(counter / 2) % 2 === 0) {
+      stateCopy.serving = true
+    } else {
+      stateCopy.serving = false
+    }
   } else {
-    stateCopy.serving = false
+    if (Math.floor(counter / 5) % 2 === 0) {
+      stateCopy.serving = true
+    } else {
+      stateCopy.serving = false
+    }
   }
 
+  // deciding who wins
   if (stateCopy.player1 >= 21 && stateCopy.player1 - stateCopy.player2 >=2) {
     stateCopy.winner = "Player 1 wins!"
   } else if (stateCopy.player2 >= 21 && stateCopy.player2 - stateCopy.player1 >=2) {
